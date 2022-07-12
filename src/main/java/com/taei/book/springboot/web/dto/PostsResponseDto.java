b8fb4c69097968bbfae324fed8c90935d26c14b2
2h5i <dev.2h5i@gmail.com>
2022-07-13 01:00:20 +0900

    게시판 완성 및 google oauth 설정 추가

---
+++ src/main/java/com/taei/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.taei.book.springboot.web.dto;
+
+import com.taei.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
