a1afde9fec5eab27b90ca5c0fa19de3490bf951a
2h5i <dev.2h5i@gmail.com>
2022-07-27 20:11:36 +0900

    일단 완성

---
+++ src/main/java/com/taei/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.taei.book.springboot.config.auth.dto;
+
+import com.taei.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
